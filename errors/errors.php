ebe22cc22085c3ec752f4c05b8cccf7c4ee9b187
dwcs-code-2223 <113686635+dwcs-code-2223@users.noreply.github.com>
2022-10-17 19:28:02 +0200

    Update errors.php
    Errata: Uso de b en lugar de c en $GLOBALS

--- errors/errors.php
+++ errors/errors.php
@@ -4,9 +4,12 @@ $a=22;
 $b= $a + "1.3€";
 echo "\$b es : $b";
 
+
+$GLOBALS["b"]=4;
+echo 'Valor de \$GLOBALS["b"] es '. $GLOBALS["b"];
+
 $c=$b/0;
 
-$GLOBALS["c"]=4;
-echo 'Valor de \$GLOBALS["c"] es '. $GLOBALS["c"];
+
 
 
